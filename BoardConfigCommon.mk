4106bacbddab36765fdf82d8db7f981d02c6521c
Jiangyi <sam.andrew.jiang@gmail.com>
2015-03-22 11:31:17 -0400

    omap4-common: Update SELinux Policies [1/2]
    Update policies, as well as commonize them here.
    
    Big thanks to Andreas B. for starting this off!
    
    Change-Id: I52c676e1ebd0bfb040cdd10eae429ee94e666fc6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,22 @@ BOARD_EGL_WORKAROUND_BUG_10194508 := true
 # Setup custom omap4xxx defines
 BOARD_USE_CUSTOM_LIBION := true
 
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/omap4-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    init.te \
+    pvrsrvinit.te \
+    radio.te \
+    rild.te \
+    wpa_supplicant.te
+
 # TI Enhancement Settings (Part 1)
 OMAP_ENHANCEMENT := true
 #OMAP_ENHANCEMENT_BURST_CAPTURE := true
